41ef4238ff4f8f24c11c5546c0068e2ab0a7e011
Antonio Cacciapuoti <yantcaccia@gmail.com>
2024-01-14 01:54:51 +0100

    route to search results to item details

--- Code/kontent/lib/kontentWidgets/kontent_download_entry.dart
+++ Code/kontent/lib/kontentWidgets/kontent_download_entry.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:html/parser.dart';
 import 'package:kontent/entities/content.dart';
+import 'package:kontent/kontentPages/mediaplayer/kontent_content_detail_page.dart';
 
 class KontentDownloadEntry extends StatelessWidget {
   final Content item;
@@ -13,57 +14,65 @@ class KontentDownloadEntry extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            width: 100,
-            height: 170,
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(item.thumbnail),
-                fit: BoxFit.fill,
+      child: GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  KontentContentDetailPageBodyWidget(content: item)),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 170,
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(item.thumbnail),
+                  fit: BoxFit.fill,
+                ),
+                color: Colors.black,
               ),
-              color: Colors.black,
             ),
-          ),
-          SizedBox(
-            height: 170,
-            width: 240,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                verticalDirection: VerticalDirection.down,
-                children: <Widget>[
-                  Text(
-                    parse(utf8.decode(item.title.codeUnits))
-                        .documentElement!
-                        .text,
-                    style: const TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Flexible(
-                    fit: FlexFit.tight,
-                    child: Text(
-                      parse(utf8.decode(item.description.codeUnits))
+            SizedBox(
+              height: 170,
+              width: 240,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  verticalDirection: VerticalDirection.down,
+                  children: <Widget>[
+                    Text(
+                      parse(utf8.decode(item.title.codeUnits))
                           .documentElement!
                           .text,
-                      overflow: TextOverflow.fade,
                       style: const TextStyle(
-                        fontSize: 15,
+                        fontSize: 25,
                       ),
                     ),
-                  ),
-                ],
+                    Flexible(
+                      fit: FlexFit.tight,
+                      child: Text(
+                        parse(utf8.decode(item.description.codeUnits))
+                            .documentElement!
+                            .text,
+                        overflow: TextOverflow.fade,
+                        style: const TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
